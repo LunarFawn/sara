a03bc8143e6dbad74ff165ef90091b46cf50c383
Jennifer Pearl <jenpearl5@gmail.com>
2023-09-24 19:28:08 -0700

    i think rna_strand is looking good

--- src/rna_squirrel/rna_strand.py
+++ src/rna_squirrel/rna_strand.py
@@ -8,37 +8,44 @@ from collections import namedtuple
 from serena.utilities.ensemble_structures import Sara2SecondaryStructure, Sara2StructureList
 
 
+
 @define
 class Energy():
-    kcal:float = 0
+    kcal: float = 0
+
+@define
+class PrimaryStructure():
+    strand: str = ''
 
 @define
 class SecondaryStructure():
-    dot_parens: str = ''
+    dot_parens: str
     free_energy: Energy = Energy()
     stack_energy: Energy = Energy()
 
 @define
 class EnergyGroup():
-    min_energy:Energy() = Energy()
-    max_energy:Energy() = Energy()
+    min_energy:Energy = Energy()
+    max_energy:Energy = Energy()
     structure_list: List[SecondaryStructure] = []
     mfe_structure:SecondaryStructure = SecondaryStructure()
     mea_structure:SecondaryStructure = SecondaryStructure()
 
 class EnsembleGroup(namedtuple):
-    start_energy: Energy
+    start_energy: float
     group:EnergyGroup
 
 @define
 class Ensemble():
+    min_energy:Energy = Energy()
+    max_energy:Energy = Energy()
     energy_groups:List[EnsembleGroup] = []
     mfe_structure:SecondaryStructure = SecondaryStructure()
     mea_structure:SecondaryStructure = SecondaryStructure()
 
 @define
 class RNAStrand():
-    primary_structure: str = ''
+    primary_structure: PrimaryStructure = PrimaryStructure()
     ensemble: Ensemble = Ensemble()
 
 
