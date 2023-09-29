5d4d678ab13f53d04e3da47dd6bf1ef1f6ec5b32
Jennifer Pearl <jenpearl5@gmail.com>
2023-09-28 20:16:57 -0700

    added nest and squirrel classes

---
+++ src/rna_squirrel/nest.py
@@ -0,0 +1,12 @@
+"""
+This is the class for defining the nests
+and how to keep track of them and access them
+"""
+
+
+
+
+
+
+class Nest():
+    pass
---
+++ src/rna_squirrel/squirrel.py
@@ -0,0 +1,9 @@
+"""
+Class for squirrling away RNA data into a db somewhere.
+Keeps track of where you keep your nests for easy access later
+"""
+
+
+
+class Squirrel:
+    pass
